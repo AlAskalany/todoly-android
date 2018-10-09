e87160b762bb45585b5efbea2e6ab052c33e54f3
Ahmed AlAskalany <ahmed.alaskalany@gmail.com>
2018-10-09 12:24:46 +0200

    Implemented MainViewModel
    Implemented MainViewModel and used it in MainFragment

--- app/src/main/java/com/alaskalany/todoly/ui/main/MainFragment.kt
+++ app/src/main/java/com/alaskalany/todoly/ui/main/MainFragment.kt
@@ -1,11 +1,13 @@
 package com.alaskalany.todoly.ui.main
 
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.alaskalany.todoly.R
 
 class MainFragment : Fragment() {
@@ -24,7 +26,7 @@ class MainFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel.data.observe(this, Observer<String> { t: String? -> view?.findViewById<TextView>(R.id.message)?.text = t })
     }
 
 }
